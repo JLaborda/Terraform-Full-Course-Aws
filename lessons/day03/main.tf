4b208a4fc45213490c5dd90335c907587a2db965
JLaborda <15078416+JLaborda@users.noreply.github.com>
2026-06-26 08:32:42 +0200

    day 3 completed

--- lessons/day03/main.tf
+++ lessons/day03/main.tf
@@ -13,11 +13,11 @@ provider "aws" {
 }
 
 # Create a S3 bucket
-resource "aws_s3_bucket" "tf_test_baivab_bucket" {
-  bucket = "my-tf-test-baiv-bucket-101"
+resource "aws_s3_bucket" "tf_test_jlaborda_bucket" {
+  bucket = "jlaborda-my-tf-test-baiv-bucket-101"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "My bucket 2.0"
     Environment = "Dev"
   }
 }
